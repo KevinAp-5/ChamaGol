203ed923ad4ce524884bc702ecc7178845405934
KevinAp-5 <keven.clash3@gmail.com>
2024-09-22 22:17:40 -0300

    App.js cleaning

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import Login from './src/components/Login/';
 import Timeline from './src/components/TimeLine/';
 import Register from './src/components/Register/';
-import Title from './src/components/Title';
 import ResetPassword from './src/components/ResetPassword';
 import HomeScreen from './src/components/HomeScreen'
 
@@ -24,14 +22,5 @@ const App = () => {
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //backgroundColor: '#e0e5e5',
-    backgroundColor: "#0C3B2E",
-    paddingTop: 0,
-  },
-});
 
 export default App;
