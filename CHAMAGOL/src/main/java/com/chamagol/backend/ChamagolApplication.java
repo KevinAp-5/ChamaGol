9c3335786c753c3de1f33bf0b39b0d499408569e
KevinAp-5 <keven.clash3@gmail.com>
2024-09-29 01:07:28 -0300

    SPRING created

---
+++ CHAMAGOL/src/main/java/com/chamagol/backend/ChamagolApplication.java
@@ -0,0 +1,13 @@
+package com.chamagol.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamagolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChamagolApplication.class, args);
+	}
+
+}
