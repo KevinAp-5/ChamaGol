a706f83d9d572dcab460465b2a454ef84a231e16
KevinAp-5 <keven.clash3@gmail.com>
2024-10-05 09:41:56 -0300

    Model created

---
+++ chamagol-spring/src/main/java/com/chamagol/model/Usuario.java
@@ -0,0 +1,49 @@
+package com.chamagol.model;
+
+import com.chamagol.enums.Assinatura;
+import com.fasterxml.jackson.databind.annotation.EnumNaming;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import com.chamagol.dto.UsuarioDTO;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "usuario")
+@Entity(name = "usuarios")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    public Usuario(UsuarioDTO usuario) {
+        this.nome = usuario.nome();
+        this.email = usuario.email();
+        this.password = usuario.password();
+        this.Assinatura = usuario.assinatura();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String nome;
+    public String email;
+    public String password;
+
+    @Enumerated(EnumType.STRING)
+    public Assinatura Assinatura;
+
+
+}
