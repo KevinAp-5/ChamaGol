7a211d7ba5a1d483cded8fe6eaf96c89268c9959
KevinAp-5 <keven.clash3@gmail.com>
2024-10-05 09:43:10 -0300

    Added controller through DTO mapping usuario

--- chamagol-spring/src/main/java/com/chamagol/controller/UsuarioController.java
+++ chamagol-spring/src/main/java/com/chamagol/controller/UsuarioController.java
@@ -1,19 +1,28 @@
 package com.chamagol.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chamagol.dto.UsuarioDTO;
+import com.chamagol.model.Usuario;
+import com.chamagol.repository.ChamaRepository;
 
 @RestController
 @RequestMapping("/user")
 public class UsuarioController {
 
+    @Autowired
+    private ChamaRepository chamaRepository;
+
     @PostMapping
-    public void testeGet(@RequestBody UsuarioDTO usuario) {
-        System.out.println(usuario);
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public void testeGet(@RequestBody UsuarioDTO dadosUsuario) {
+        chamaRepository.save(new Usuario(dadosUsuario));
     }
+
 }
