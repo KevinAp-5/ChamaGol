d00cc86bb1bc9dc73bd5eabcf7ff8aa29fbb0898
KevinAp-5 <keven.clash3@gmail.com>
2024-10-14 10:51:30 -0300

    400 status code handler

--- chamagol-spring/src/main/java/com/chamagol/infra/TrataErros.java
+++ chamagol-spring/src/main/java/com/chamagol/infra/TrataErros.java
@@ -1,8 +1,14 @@
 package com.chamagol.infra;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -11,8 +17,24 @@ public class TrataErros {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Object> trata404() {
-
         return ResponseEntity.notFound().build();
+    }
 
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<Map<String, String>> trata400(HandlerMethodValidationException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        // Itera sobre todos os erros de validação
+        ex.getAllErrors().forEach(error -> {
+            if (error instanceof FieldError) {
+                String fieldName = ((FieldError) error).getField();
+                String errorMessage = error.getDefaultMessage();
+                errors.put(fieldName, errorMessage);
+            } else {
+                errors.put("Error: ", error.getDefaultMessage());
+            }
+        });
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
