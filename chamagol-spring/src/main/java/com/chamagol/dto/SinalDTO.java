5dbd8b09ffee75df69928d4417c9b3ba4a6ff796
KevinAp-5 <keven.clash3@gmail.com>
2024-10-18 08:58:22 -0300

    Sinal DTO

---
+++ chamagol-spring/src/main/java/com/chamagol/dto/SinalDTO.java
@@ -0,0 +1,28 @@
+package com.chamagol.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record SinalDTO(
+    @JsonProperty("id")
+
+    Long id,
+
+    @NotBlank
+    String campeonato,
+
+    @NotBlank
+    String times,
+
+    @NotBlank
+    String tempoPartida,
+
+    @NotBlank
+    String placar,
+
+    @NotBlank
+    String acaoSinal
+
+) {
+}
