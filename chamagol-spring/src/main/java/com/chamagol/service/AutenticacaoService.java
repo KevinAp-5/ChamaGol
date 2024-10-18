7aa07a2ce54ebd6a16758e095a938db484a6421f
KevinAp-5 <keven.clash3@gmail.com>
2024-10-18 09:44:13 -0300

    Auth class

---
+++ chamagol-spring/src/main/java/com/chamagol/service/AutenticacaoService.java
@@ -0,0 +1,30 @@
+package com.chamagol.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.chamagol.repository.UsuarioRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService{
+
+    private UsuarioRepository usuarioRepository;
+
+    public AutenticacaoService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails user = usuarioRepository.findByNome(username);
+
+        if (user == null) {
+            throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        }
+
+        return user;
+    }
+
+}
