3d6c31d9c41c81af5fb77f3d207655a3b216ce82
KevinAp-5 <keven.clash3@gmail.com>
2024-10-21 10:21:36 -0300

    Removed manual UserDetail mapping

--- chamagol-spring/src/main/java/com/chamagol/service/AutenticacaoService.java
+++ chamagol-spring/src/main/java/com/chamagol/service/AutenticacaoService.java
@@ -1,15 +1,10 @@
 package com.chamagol.service;
 
-import java.util.Collections;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
-import com.chamagol.model.Usuario;
 import com.chamagol.repository.UsuarioRepository;
 
 @Service
@@ -23,21 +18,7 @@ public class AutenticacaoService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) {
-        var user = usuarioRepository.findByEmail(username);
-        return userMappingUserDetails(user);
-    }
-
-    private UserDetails userMappingUserDetails(Usuario usuario) {
-        GrantedAuthority userRole = new SimpleGrantedAuthority("ROLE_USER");
-
-        return new User(
-                usuario.getNome(),
-                usuario.getSenha(),
-                true,
-                true,
-                true,
-                true,
-                Collections.singletonList(userRole));
+        return usuarioRepository.findByEmail(username);
     }
 
 }
