0da7cab52044208dcfcd9da9aaada1a5277c2654
KevinAp-5 <keven.clash3@gmail.com>
2024-10-27 22:15:19 -0300

    SecurityConfiguration update
    endpoint login free

--- chamagol-spring/src/main/java/com/chamagol/infra/SecurityConfigurations.java
+++ chamagol-spring/src/main/java/com/chamagol/infra/SecurityConfigurations.java
@@ -2,24 +2,38 @@ package com.chamagol.infra;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations {
+	private SecurityFilter securityFilter;
+
+	public SecurityConfigurations(SecurityFilter securityFilter) {
+		this.securityFilter = securityFilter;
+	}
+
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		return httpSecurity.csrf(csrf -> csrf.disable())
-				.sessionManagement(
-						management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+		return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+				.sessionManagement(management -> management
+						.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(requests -> requests
+						.requestMatchers(HttpMethod.POST, "/login")
+						.permitAll()
+						.anyRequest().authenticated())
+				.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
 				.build();
 	}
 
