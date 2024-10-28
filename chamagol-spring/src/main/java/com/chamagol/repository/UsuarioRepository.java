6b9e7af96427cbe81b5c16edc49f7b1c4fd640f7
KevinAp-5 <keven.clash3@gmail.com>
2024-10-27 23:05:41 -0300

    Returning Optional

--- chamagol-spring/src/main/java/com/chamagol/repository/UsuarioRepository.java
+++ chamagol-spring/src/main/java/com/chamagol/repository/UsuarioRepository.java
@@ -1,6 +1,7 @@
 package com.chamagol.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,7 +14,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
     List<Usuario> findByStatus(Status status);
 
-    UserDetails findByEmail(String nome);
+    Optional<UserDetails> findByEmail(String email);
 
     boolean existsByEmail(String email);
 }
