277f6bb7f81c8d5fcf24fe7c490f7859243990ca
KevinAp-5 <keven.clash3@gmail.com>
2024-10-27 22:16:07 -0300

    SecurityFilter updated

--- chamagol-spring/src/main/java/com/chamagol/infra/SecurityFilter.java
+++ chamagol-spring/src/main/java/com/chamagol/infra/SecurityFilter.java
@@ -3,8 +3,13 @@ package com.chamagol.infra;
 import java.io.IOException;
 
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.chamagol.repository.UsuarioRepository;
+import com.chamagol.service.TokenService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -13,11 +18,43 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+    private TokenService tokenService;
+    private UsuarioRepository usuarioRepository;
+
+    public SecurityFilter(TokenService tokenService, UsuarioRepository usuarioRepository) {
+        this.tokenService = tokenService;
+        this.usuarioRepository = usuarioRepository;
+    }
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
-            @NonNull FilterChain filterChain)
-            throws ServletException, IOException {   
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        var token = pegarToken(request);
+        if (token != null) {
+            var subject = tokenService.getSubject(token);
+            var usuario = usuarioRepository.findByEmail(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(
+                usuario,
+                null,
+                usuario.getAuthorities()
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
         filterChain.doFilter(request, response);
+    }
+
+    private String pegarToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) {
+            return null;
+        }
+
+        String[] tokenArray = authHeader.split(" ");
+
+        return tokenArray[1];
     }
 }
