71ecae7c6b406a6a385f77ce032ca55168147246
KevinAp-5 <keven.clash3@gmail.com>
2024-10-31 07:10:51 -0300

    Creation e migration of new entity

---
+++ chamagol-spring/src/main/java/com/chamagol/entity/UsuarioVerificadorTokenEntity.java
@@ -0,0 +1,39 @@
+package com.chamagol.entity;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import com.chamagol.model.Usuario;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "UsuarioVerificador")
+@Table(name = "usuarioVerificador")
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class UsuarioVerificadorTokenEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private UUID uuid;
+
+    @Column(nullable = false)
+    private Instant dataExpira;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_USUARIO", referencedColumnName = "ID", unique = true)
+    private Usuario usuario;
+}
