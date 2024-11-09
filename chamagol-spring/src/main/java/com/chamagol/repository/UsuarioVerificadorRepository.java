bae71af997e4c105f16c8a6f7f7f5d2af97641c6
KevinAp-5 <keven.clash3@gmail.com>
2024-11-09 08:39:50 -0300

    repository update

--- chamagol-spring/src/main/java/com/chamagol/repository/UsuarioVerificadorRepository.java
+++ chamagol-spring/src/main/java/com/chamagol/repository/UsuarioVerificadorRepository.java
@@ -1,11 +1,14 @@
 package com.chamagol.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.chamagol.model.UsuarioVerificadorEntity;
 
 public interface UsuarioVerificadorRepository extends JpaRepository<UsuarioVerificadorEntity, Long>{
-    
+    Optional<UsuarioVerificadorEntity> findByUuid(UUID uuid);
 }
 
     
