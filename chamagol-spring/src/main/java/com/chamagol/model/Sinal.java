571d4474af3b53e1dd7bf9161443240b5db82608
KevinAp-5 <keven.clash3@gmail.com>
2024-11-13 10:58:19 -0300

    model update

--- chamagol-spring/src/main/java/com/chamagol/model/Sinal.java
+++ chamagol-spring/src/main/java/com/chamagol/model/Sinal.java
@@ -1,14 +1,19 @@
 package com.chamagol.model;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -21,25 +26,36 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(of = "id")
 public class Sinal {
 
-    @NotNull
     @JsonProperty("_id")
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank
+    @Length(min = 2, max = 255)
+    @Column(nullable = false)
     private String campeonato;
 
-    @NotBlank
+    @Length(min = 2, max = 255)
+    @Column(nullable = false)
     private String nomeTimes;
 
     @NotBlank
+    @Length(min = 2, max = 255)
+    @Column(nullable = false)
     private String tempoPartida;
 
     @NotBlank
+    @Length(min = 2, max = 255)
+    @Column(nullable = false)
     private String placar;
 
     @NotBlank
+    @Length(min = 2, max = 255)
+    @Column(nullable = false)
     private String acaoSinal;
 
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
