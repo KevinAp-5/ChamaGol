85ebf7e588be39136f83a2263bcf7112ab32794e
KevinAp-5 <keven.clash3@gmail.com>
2024-11-15 08:46:59 -0300

    repository sinal update

--- chamagol-spring/src/main/java/com/chamagol/repository/SinalRepository.java
+++ chamagol-spring/src/main/java/com/chamagol/repository/SinalRepository.java
@@ -3,6 +3,10 @@ package com.chamagol.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.chamagol.model.Sinal;
+import java.util.List;
+import com.chamagol.enums.Status;
+
 
 public interface SinalRepository extends JpaRepository<Sinal, Long> {
+    List<Sinal> findByStatus(Status status);
 }
