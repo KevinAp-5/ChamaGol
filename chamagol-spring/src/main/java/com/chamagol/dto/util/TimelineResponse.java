d42f16d854c651cd98ebab9f5ee4799597dbde46
KevinAp-5 <keven.clash3@gmail.com>
2024-11-17 20:13:40 -0300

    TimelineResponse DTO

---
+++ chamagol-spring/src/main/java/com/chamagol/dto/util/TimelineResponse.java
@@ -0,0 +1,32 @@
+package com.chamagol.dto.util;
+
+import java.time.LocalDateTime;
+
+import com.chamagol.model.Sinal;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(of = "id")
+public class TimelineResponse {
+    private Long id;
+    private String campeonato;
+    private String nomeTimes;
+    private String placar;
+    private String tempoPartida;
+    private String acaoSinal;
+    // private String tipoEvento;
+    private LocalDateTime createdAt;
+
+    public TimelineResponse(Sinal sinal) {
+        this.id = sinal.getId();
+        this.campeonato = sinal.getCampeonato();
+        this.nomeTimes = sinal.getNomeTimes();
+        this.placar = sinal.getPlacar();
+        this.tempoPartida = sinal.getTempoPartida();
+        this.acaoSinal = sinal.getAcaoSinal();
+        // this.tipoEvento = tipoEvento;
+        this.createdAt = sinal.getCreatedAt();
+    }
+}
