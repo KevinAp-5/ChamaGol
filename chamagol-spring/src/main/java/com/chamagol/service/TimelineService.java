3ac257c958360146b2e00276b318b07d5f637063
KevinAp-5 <keven.clash3@gmail.com>
2024-11-17 20:13:29 -0300

    TimelineService

---
+++ chamagol-spring/src/main/java/com/chamagol/service/TimelineService.java
@@ -0,0 +1,27 @@
+package com.chamagol.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.chamagol.dto.util.TimelineResponse;
+import com.chamagol.enums.Status;
+import com.chamagol.model.Sinal;
+import com.chamagol.repository.SinalRepository;
+
+@Service
+public class TimelineService {
+    private final SinalRepository sinalRepository;
+
+    public TimelineService(SinalRepository sinalRepository) {
+        this.sinalRepository = sinalRepository;
+    }
+
+    public List<TimelineResponse> getTimeline() {
+        List<Sinal> sinais = sinalRepository.findByStatus(Status.ACTIVE);
+
+        // Converte para TimelineResponse
+        return sinais.stream().map(TimelineResponse::new).collect(Collectors.toList());
+    }
+}
