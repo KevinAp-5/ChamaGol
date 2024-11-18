3df25a3a0f359580a1b3b2fbcbbbbcff048846a2
KevinAp-5 <keven.clash3@gmail.com>
2024-11-18 06:50:57 -0300

    repository update

--- chamagol-spring/src/main/java/com/chamagol/repository/SinalRepository.java
+++ chamagol-spring/src/main/java/com/chamagol/repository/SinalRepository.java
@@ -1,12 +1,21 @@
 package com.chamagol.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.chamagol.model.Sinal;
 import java.util.List;
-import com.chamagol.enums.Status;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.chamagol.enums.Status;
+import com.chamagol.model.Sinal;
 
 public interface SinalRepository extends JpaRepository<Sinal, Long> {
     List<Sinal> findByStatus(Status status);
+
+    @Query(
+        value = "SELECT * FROM sinal WHERE status = 'ACTIVE' AND tipo_evento = :tipoEvento ORDER BY created_at DESC;",
+        nativeQuery = true
+    )
+    List<Sinal> findByTipoEvento(@Param("tipoEvento") String tipoEvento);
+
 }
