16834e1fc9b17e079e79581bfb9ae3f88d724445
KevinAp-5 <keven.clash3@gmail.com>
2024-11-25 08:45:48 -0300

    new cache services

---
+++ chamagol-spring/src/main/java/com/chamagol/service/UsuarioCacheService.java
@@ -0,0 +1,23 @@
+package com.chamagol.service;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class UsuarioCacheService {
+    private final UsuarioService usuarioService;
+
+    public UsuarioCacheService(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @Cacheable(value = "usuario", key = "#email")
+    public UserDetails getUsuarioFromCache(String email) {
+        log.debug("Cache miss para usuário: {}", email);
+        return usuarioService.getUsuario(email);
+    }
+}
