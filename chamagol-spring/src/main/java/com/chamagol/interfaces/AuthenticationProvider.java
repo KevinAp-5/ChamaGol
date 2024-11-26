93709e42fb3f0f313c7a0ab3fb0bbe76fa69e282
KevinAp-5 <keven.clash3@gmail.com>
2024-11-25 21:48:09 -0300

    interface update cache

--- chamagol-spring/src/main/java/com/chamagol/interfaces/AuthenticationProvider.java
+++ chamagol-spring/src/main/java/com/chamagol/interfaces/AuthenticationProvider.java
@@ -2,6 +2,8 @@ package com.chamagol.interfaces;
 
 import org.springframework.security.core.userdetails.UserDetails;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
 public interface AuthenticationProvider {
     UserDetails loadUserByUsername(String username);
 }
