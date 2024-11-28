5745218e6faea6ae224ea48e19ab646266a0162f
KevinAp-5 <keven.clash3@gmail.com>
2024-11-28 10:13:28 -0300

    repository update

--- chamagol-spring/src/main/java/com/chamagol/repository/UsuarioRepository.java
+++ chamagol-spring/src/main/java/com/chamagol/repository/UsuarioRepository.java
@@ -1,8 +1,9 @@
 package com.chamagol.repository;
 
-import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
@@ -13,7 +14,7 @@ import com.chamagol.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-    List<Usuario> findByStatus(Status status);
+    Page<Usuario> findByStatus(Status status, Pageable pageable);
 
     Optional<UserDetails> findByEmail(String email);
 
