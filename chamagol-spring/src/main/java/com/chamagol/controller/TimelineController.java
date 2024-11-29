ac330cfdcadb581f7aa7049e41aa536f9422cd1b
KevinAp-5 <keven.clash3@gmail.com>
2024-11-29 09:27:07 -0300

    Controller update

--- chamagol-spring/src/main/java/com/chamagol/controller/TimelineController.java
+++ chamagol-spring/src/main/java/com/chamagol/controller/TimelineController.java
@@ -1,7 +1,10 @@
 package com.chamagol.controller;
 
+import java.time.Duration;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +15,8 @@ import com.chamagol.dto.util.TimelineResponse;
 import com.chamagol.enums.TipoEvento;
 import com.chamagol.service.util.TimelineService;
 
+import reactor.core.publisher.Flux;
+
 @RestController
 @RequestMapping("/api/timeline")
 public class TimelineController {
@@ -20,6 +25,13 @@ public class TimelineController {
 
     public TimelineController(TimelineService timelineService) {
         this.timelineService = timelineService;
+    }
+
+    @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<TimelineResponse> streamTimeline() {
+        // Atualizações a cada 1 segundo, enviando novos sinais
+        return Flux.interval(Duration.ofSeconds(1))
+                   .flatMap(i -> timelineService.getLatestUpdates());
     }
 
     @GetMapping
