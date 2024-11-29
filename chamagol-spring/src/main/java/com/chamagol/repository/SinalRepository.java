69a0b31962d92d850db593628bd345e213d26ea8
KevinAp-5 <keven.clash3@gmail.com>
2024-11-29 09:27:01 -0300

    repo update

--- chamagol-spring/src/main/java/com/chamagol/repository/SinalRepository.java
+++ chamagol-spring/src/main/java/com/chamagol/repository/SinalRepository.java
@@ -1,11 +1,13 @@
 package com.chamagol.repository;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.domain.Pageable;
 
 import com.chamagol.enums.Status;
 import com.chamagol.enums.TipoEvento;
@@ -19,4 +21,10 @@ public interface SinalRepository extends JpaRepository<Sinal, Long> {
         value = "SELECT s FROM Sinal s WHERE s.status = 'ACTIVE' AND s.tipoEvento = :tipoEvento ORDER BY s.createdAt DESC"
     )
     Page<Sinal> findByTipoEvento(@Param("tipoEvento") TipoEvento tipoEvento, Pageable pageable);
+
+
+    @Query(
+        value = "SELECT s FROM Sinal s WHERE s.status = 'ACTIVE' ORDER BY s.createdAt DESC FETCH FIRST 10 ROWS ONLY"
+    )
+    List<Sinal> findTop10ByOrderByCreatedAtDesc();
 }
