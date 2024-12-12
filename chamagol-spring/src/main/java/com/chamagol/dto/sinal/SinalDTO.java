c63583542398e14749aabc75143fef5af584be2e
KevinAp-5 <keven.clash3@gmail.com>
2024-12-12 08:54:04 -0300

    refact: Sinal DTO

--- chamagol-spring/src/main/java/com/chamagol/dto/sinal/SinalDTO.java
+++ chamagol-spring/src/main/java/com/chamagol/dto/sinal/SinalDTO.java
@@ -1,44 +1,23 @@
 package com.chamagol.dto.sinal;
 
 import java.time.LocalDateTime;
-
-import com.chamagol.dto.sinal.converter.TipoEventoConverter;
 import com.chamagol.enums.TipoEvento;
 import com.chamagol.model.Sinal;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.persistence.Convert;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.validation.constraints.NotBlank;
-
 public record SinalDTO(
-
     @JsonProperty("id")
     Long id,
 
-    @NotBlank
     String campeonato,
-
-    @NotBlank
     String nomeTimes,
-
-    @NotBlank
     String tempoPartida,
-
-    @NotBlank
     String placar,
-
-    @NotBlank
     String acaoSinal,
-
     LocalDateTime created_at,
 
     @JsonProperty("tipoEvento")
-    @Enumerated(EnumType.STRING)
-    @Convert(converter = TipoEventoConverter.class)
     TipoEvento tipoEvento
-
 ) {
     public SinalDTO(Sinal sinal) {
         this(
