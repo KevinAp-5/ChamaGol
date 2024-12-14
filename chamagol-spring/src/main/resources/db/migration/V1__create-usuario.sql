dc607e428631741623acb3d06f25c29d072ba0ab
KevinAp-5 <keven.clash3@gmail.com>
2024-12-14 08:09:21 -0300

    fix: migration fix

--- chamagol-spring/src/main/resources/db/migration/V1__create-usuario.sql
+++ chamagol-spring/src/main/resources/db/migration/V1__create-usuario.sql
@@ -3,6 +3,7 @@ CREATE table usuario (
     nome varchar(120) not null,
     email varchar(120) not null,
     senha varchar(100) not null,
-    status varchar(24) not null DEFAULT "INACTIVE",
-    role varchar(20) not null DEFAULT "USER"
+    status varchar(24) not null,
+    assinatura varchar(100) not null,
+    user_role varchar(20) not null
 )
