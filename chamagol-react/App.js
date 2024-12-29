dfd5340edb709cbe8ccabf9e9d6c3a138066cabb
KevinAp-5 <keven.clash3@gmail.com>
2024-12-29 17:45:51 -0300

    feat: App update and dependencies update'

--- chamagol-react/App.js
+++ chamagol-react/App.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import "@expo/metro-runtime";
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import React from 'react';
+import EmailConfirmation from './src/components/EmailConfirm';
+import HomeScreen from './src/components/HomeScreen';
 import Login from './src/components/Login/';
-import Timeline from './src/components/TimeLine/';
 import Register from './src/components/Register/';
 import ResetPassword from './src/components/ResetPassword';
-import HomeScreen from './src/components/HomeScreen'
-import EmailConfirmation from './src/components/EmailConfirm';
-import "@expo/metro-runtime";
+import Timeline from './src/components/TimeLine/';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name= "EmailConfirmation" component={EmailConfirmation} options={{headerShown: false}} />
-        <Stack.Screen name= "Home" component={HomeScreen} options={{headerShown: false}} />
-        <Stack.Screen name= "Login" component={Login} options={{headerShown: false}} />
-        <Stack.Screen name= "Register" component={Register} options={{headerShown: false}} />
-        <Stack.Screen name= "ResetPassword" component={ResetPassword} options={{headerShown: false}} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={{gestureEnabled: true}}>
+        <Stack.Screen name= "EmailConfirmation" component={EmailConfirmation} options={{headerShown: false, gestureDirection: 'horizontal'}} />
+        <Stack.Screen name= "Home" component={HomeScreen} options={{headerShown: false, gestureDirection: 'horizontal'}} />
+        <Stack.Screen name= "Login" component={Login} options={{headerShown: false, gestureDirection: 'horizontal'}} />
+        <Stack.Screen name= "Register" component={Register} options={{headerShown: false, gestureDirection: 'horizontal'}} />
+        <Stack.Screen name= "ResetPassword" component={ResetPassword} options={{headerShown: false, gestureDirection: 'horizontal'}} />
         <Stack.Screen name= "Timeline" component={Timeline} />
       </Stack.Navigator>
     </NavigationContainer>
