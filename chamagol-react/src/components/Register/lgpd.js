f3350cf3396eb5ab29c5bb2316ddca0b827323a3
KevinAp-5 <keven.clash3@gmail.com>
2025-01-06 23:18:46 -0300

    docs: lgpd terms

---
+++ chamagol-react/src/components/Register/lgpd.js
@@ -0,0 +1,21 @@
+const lgpd_text = `Política de Privacidade e Proteção de Dados Pessoais
+A presente Política de Privacidade tem como objetivo esclarecer como o "Chamagol" (doravante denominado "Aplicativo") coleta, utiliza, armazena e compartilha os dados pessoais dos usuários, em conformidade com a Lei Geral de Proteção de Dados Pessoais (LGPD - Lei nº 13.709/2018).
+1. Coleta e Uso de Dados Pessoais
+O Aplicativo coleta dados pessoais dos usuários para as seguintes finalidades:
+Armazenamento de Dados: Os dados pessoais são armazenados em servidores seguros e utilizados para identificação dos usuários, garantindo a autenticidade nas interações com o Aplicativo.
+Envio de E-mails: Utilizamos os dados coletados para enviar comunicações importantes, como confirmações de cadastro, atualizações e informações sobre o uso do Aplicativo.
+Verificações de Autenticidade: Para garantir a segurança e a veracidade das informações, realizamos verificações de autenticidade que podem envolver o uso de dados pessoais.
+2. Segurança dos Dados
+O Aplicativo adota medidas técnicas e administrativas adequadas para proteger os dados pessoais contra acessos não autorizados, vazamentos e outras situações que possam comprometer a integridade e a confidencialidade das informações.
+3. Retenção de Dados
+Os dados pessoais serão retidos pelo tempo necessário para cumprir as finalidades para as quais foram coletados, respeitando sempre a legislação vigente. Após esse período, os dados serão eliminados ou anonimizados.
+4. Direitos dos Usuários
+Os usuários têm o direito de acessar, corrigir ou solicitar a exclusão de seus dados pessoais a qualquer momento. Para exercer esses direitos, os usuários podem entrar em contato conosco através dos canais disponibilizados no Aplicativo.
+5. Isenção de Responsabilidade
+O "Chamagol" não é um aplicativo de apostas. O Aplicativo é destinado exclusivamente à oferta de dicas sobre futebol, visando proporcionar aos usuários informações relevantes e atualizadas sobre o esporte.
+6. Alterações na Política de Privacidade
+Esta Política de Privacidade pode ser atualizada periodicamente. Recomendamos que os usuários revisem esta política regularmente para se manterem informados sobre como estamos protegendo suas informações.
+7. Contato
+Para quaisquer dúvidas ou solicitações relacionadas à proteção de dados pessoais, entre em contato conosco através do e-mail [keven.clash3@gmail.com]. Ao utilizar o "Chamagol", você concorda com os termos desta Política de Privacidade e consente com o tratamento dos seus dados pessoais conforme descrito acima.`
+
+export default lgpd_text;
