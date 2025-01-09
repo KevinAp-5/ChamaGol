1984474e3bb977d7da5505a96c5d3596356f5dc8
KevinAp-5 <keven.clash3@gmail.com>
2025-01-09 09:36:36 -0300

    feat: Controle email repository created

---
+++ chamagol-spring/src/main/java/com/chamagol/repository/ControleEmailRepository.java
@@ -0,0 +1,9 @@
+package com.chamagol.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.chamagol.model.ControleEmail;
+
+public interface ControleEmailRepository extends JpaRepository<ControleEmail, Long>{
+
+}
