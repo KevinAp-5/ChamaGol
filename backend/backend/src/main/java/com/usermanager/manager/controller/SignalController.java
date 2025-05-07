954392b465c4f1fd18d797b780b93e217ef47ce9
KevinAp-5 <keven.clash3@gmail.com>
2025-05-06 23:07:16 -0300

    feat: SignalController + Create and Get methods

---
+++ backend/backend/src/main/java/com/usermanager/manager/controller/SignalController.java
@@ -0,0 +1,45 @@
+package com.usermanager.manager.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.usermanager.manager.dto.signal.SignalCreated;
+import com.usermanager.manager.dto.signal.SignalDTO;
+import com.usermanager.manager.service.signals.SignalService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("api/signals")
+@Slf4j
+public class SignalController {
+    private final SignalService signalsService;
+
+    public SignalController(SignalService signalsService) {
+        this.signalsService = signalsService;
+    }
+
+    // Create
+    @PostMapping
+    public ResponseEntity<SignalCreated> createSignal(@RequestBody SignalDTO data) {
+        log.info("signal {}", data);
+        SignalCreated response = signalsService.createSignal(data);
+        return ResponseEntity.created(UriComponentsBuilder.fromPath("/api/signals")
+                .path("/{id}")
+                .buildAndExpand(response.id())
+                .toUri())
+                .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SignalDTO>> getAllSignals() {
+        return ResponseEntity.ok(signalsService.getSignal());
+    }
+}
