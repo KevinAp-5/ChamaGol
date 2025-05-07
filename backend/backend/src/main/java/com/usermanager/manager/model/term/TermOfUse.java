552c237dcdfd2fc07dd93537e88e7c1682709dc0
KevinAp-5 <keven.clash3@gmail.com>
2025-05-07 01:39:20 -0300

    feat: TermOfUse entity created

---
+++ backend/backend/src/main/java/com/usermanager/manager/model/term/TermOfUse.java
@@ -0,0 +1,40 @@
+package com.usermanager.manager.model.term;
+
+import java.time.ZonedDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "term_of_use")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TermOfUse {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String version;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private ZonedDateTime createdAt;
+}
