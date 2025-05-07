14d4ded342826fb5c97b91a9e60abbd3921fc159
KevinAp-5 <keven.clash3@gmail.com>
2025-05-06 23:04:22 -0300

    feat: signal DTOS

---
+++ backend/backend/src/main/java/com/usermanager/manager/dto/signal/SignalDTO.java
@@ -0,0 +1,15 @@
+package com.usermanager.manager.dto.signal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record SignalDTO(
+    @NotNull @Positive Long id,
+    @NotBlank String campeonato, 
+    @NotBlank String nomeTimes, 
+    @NotBlank String tempoPartida, 
+    @NotBlank String placar, 
+    @NotBlank String acaoSinal) {
+
+}
