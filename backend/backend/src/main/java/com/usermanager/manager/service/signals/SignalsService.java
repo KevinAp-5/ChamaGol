12e06889d8fc735f61d60ba77129aad846f66fc5
KevinAp-5 <keven.clash3@gmail.com>
2025-05-06 23:05:23 -0300

    feat: SignalsService update

--- backend/backend/src/main/java/com/usermanager/manager/service/signals/SignalsService.java
+++ backend/backend/src/main/java/com/usermanager/manager/service/signals/SignalsService.java
@@ -1,5 +1,13 @@
 package com.usermanager.manager.service.signals;
 
+import java.util.List;
+
+import com.usermanager.manager.dto.signal.SignalCreated;
+import com.usermanager.manager.dto.signal.SignalDTO;
+
 public interface SignalsService {
 
+    public SignalCreated createSignal(SignalDTO data);
+
+    public List<SignalDTO> getSignal();
 }
--- backend/backend/src/main/java/com/usermanager/manager/service/signals/impl/SignalsServiceImpl.java
+++ backend/backend/src/main/java/com/usermanager/manager/service/signals/impl/SignalsServiceImpl.java
@@ -1,10 +1,40 @@
 package com.usermanager.manager.service.signals.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.usermanager.manager.dto.signal.SignalCreated;
+import com.usermanager.manager.dto.signal.SignalDTO;
+import com.usermanager.manager.mappers.SignalMapper;
+import com.usermanager.manager.model.signal.Signal;
+import com.usermanager.manager.repository.SignalsRepository;
 import com.usermanager.manager.service.signals.SignalsService;
 
+import jakarta.validation.Valid;
+
 @Service
-public class SignalsServiceImpl implements SignalsService{
+public class SignalsServiceImpl implements SignalsService {
+    private final SignalsRepository signalsRepository;
+    private final SignalMapper signalMapper;
+
+    public SignalsServiceImpl(SignalsRepository signalsRepository, SignalMapper signalMapper) {
+        this.signalsRepository = signalsRepository;
+        this.signalMapper = signalMapper;
+    }
+
+    @Override
+    public SignalCreated createSignal(@Valid SignalDTO data) {
+        Signal signal = signalMapper.signalDTOToEntity(data);
+        signal = signalsRepository.save(signal);
+        return signalMapper.entityToSignalCreated(signal);
+    }
+
+    @Override
+    public List<SignalDTO> getSignal() {
+        return signalsRepository.findAll().stream()
+            .map(signalMapper::entityToSignalDTO)
+            .toList();
+    }
 
 }
