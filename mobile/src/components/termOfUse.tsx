1f9a4586107cf4503f922109515db18a44cb3eaf
KevinAp-5 <keven.clash3@gmail.com>
2025-05-07 22:42:21 -0300

    feat: TermOfUse function to fetch terms of use

---
+++ mobile/src/components/termOfUse.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { api, ApiResponse } from '../config/Api';
+import { Alert } from "react-native";
+
+export async function fetchTerm(): Promise<string | undefined> {
+  try {
+    const response = await api("GET", "terms/latest");
+
+    if (response.status === 200) {
+      return (response.data as { content: string })?.content;
+    } else {
+      Alert.alert("Erro", (response.data as { message: string })?.message || "Erro de requisição.");
+      return undefined;
+    }
+  } catch (error: any) {
+    Alert.alert("Erro", error?.response?.data?.message || "Erro de requisição, tente mais tarde.");
+    return undefined;
+  }
+}
