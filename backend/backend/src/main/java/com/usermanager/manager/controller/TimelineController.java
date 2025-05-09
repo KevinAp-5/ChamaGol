5e614822bab2fcba751be861a2e9a0c32cadd177
KevinAp-5 <keven.clash3@gmail.com>
2025-05-09 00:03:52 -0300

    feat: TimelineController for sending signals to front

---
+++ backend/backend/src/main/java/com/usermanager/manager/controller/TimelineController.java
@@ -0,0 +1,55 @@
+// ChatController.java
+package com.usermanager.manager.controller;
+
+import java.time.ZonedDateTime;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+
+import com.usermanager.manager.dto.common.ChatMessage;
+import com.usermanager.manager.enums.Status;
+import com.usermanager.manager.model.signal.Signal;
+
+@Controller
+@EnableScheduling
+public class TimelineController {
+
+    private final SimpMessagingTemplate messagingTemplate;
+    private final AtomicLong counter = new AtomicLong(1);
+
+    public TimelineController(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    // Apenas ADMIN pode enviar mensagens pelo WebSocket
+    @PreAuthorize("hasRole('ADMIN')")
+    @MessageMapping("/chat")
+    @SendTo("/topic/messages")
+    public ChatMessage handleMessage(ChatMessage message) {
+        return message;
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void scheduleMessage() {
+        long id = counter.getAndIncrement();
+
+        Signal signal = Signal.builder()
+                .id(id)
+                .campeonato("Campeonato Brasileiro")
+                .nomeTimes("Flamengo x Palmeiras")
+                .tempoPartida("75'")
+                .placar("2x1")
+                .acaoSinal("Aposta: Vitória do Flamengo")
+                .createdAt(ZonedDateTime.now())
+                .status(Status.ACTIVE)
+                .build();
+
+        messagingTemplate.convertAndSend("/topic/messages", signal);
+    }
+}
