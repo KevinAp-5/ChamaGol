06bf52abcce9f4f65ff6e091d8c099544a964543
KevinAp-5 <keven.clash3@gmail.com>
2025-05-12 00:24:03 -0300

    feat: adding websocket interceptor

--- backend/backend/src/main/java/com/usermanager/manager/infra/config/WebSocketConfig.java
+++ backend/backend/src/main/java/com/usermanager/manager/infra/config/WebSocketConfig.java
@@ -2,14 +2,26 @@ package com.usermanager.manager.infra.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import com.usermanager.manager.infra.security.token.TokenService;
+import com.usermanager.manager.infra.security.websocket.WebSocketAuthInterceptor;
+import com.usermanager.manager.service.user.UserService;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final TokenService tokenService;
+    private final UserService userService;
+
+    public WebSocketConfig(TokenService tokenService, UserService userService) {
+        this.tokenService = tokenService;
+        this.userService = userService;
+    }
 
     @Override
     public void configureMessageBroker(@NonNull MessageBrokerRegistry config) {
@@ -19,6 +31,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
     @Override
     public void registerStompEndpoints(@NonNull StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws/chat")
+                .setAllowedOriginPatterns("*")
+                .addInterceptors(new WebSocketAuthInterceptor(tokenService, userService))
+                .withSockJS();
     }
+
+    @Override
+    public void configureClientInboundChannel(@NonNull ChannelRegistration registration) {
+        registration.interceptors(new WebSocketAuthInterceptor(tokenService, userService));
+    }
+
 }
