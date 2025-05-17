c95d2a99f0044704693f7e6df824e65cf9b8595f
KevinAp-5 <keven.clash3@gmail.com>
2025-05-16 21:41:27 -0300

    feat: Preference client bean to avoid mock

--- backend/backend/src/main/java/com/usermanager/manager/infra/config/MercadoPagoConfig.java
+++ backend/backend/src/main/java/com/usermanager/manager/infra/config/MercadoPagoConfig.java
@@ -1,18 +1,26 @@
 package com.usermanager.manager.infra.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.mercadopago.client.preference.PreferenceClient;
 
 import jakarta.annotation.PostConstruct;
 
 @Configuration
 public class MercadoPagoConfig {
-    
+
     @Value("${mercadopago.access.token}")
     private String accessToken;
 
     @PostConstruct
     public void init() {
         com.mercadopago.MercadoPagoConfig.setAccessToken(accessToken);
+    }
+
+    @Bean
+    public PreferenceClient getPreferenceClient() {
+        return new PreferenceClient();
     }
 }
