afe293a07f79c556c185e89ea00b1e48dab923a7
KevinAp-5 <keven.clash3@gmail.com>
2025-05-20 00:28:10 -0300

    feat: refreshTokenRepo new method to delete all used or expired tokens

--- backend/backend/src/main/java/com/usermanager/manager/repository/RefreshTokenRepository.java
+++ backend/backend/src/main/java/com/usermanager/manager/repository/RefreshTokenRepository.java
@@ -1,10 +1,14 @@
 package com.usermanager.manager.repository;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.usermanager.manager.model.security.RefreshToken;
 import com.usermanager.manager.model.user.User;
@@ -19,4 +23,9 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID
     void deleteByUser(User user);
 
     void deleteByToken(String token);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM RefreshToken rt WHERE rt.used = true OR rt.createdAt < :threshold")
+    void deleteAllUsedAndExpired(Instant threshold);
 }
