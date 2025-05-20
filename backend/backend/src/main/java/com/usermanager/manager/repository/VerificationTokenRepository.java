db53c92302baf2634b032a4e7603a934195058d8
KevinAp-5 <keven.clash3@gmail.com>
2025-05-20 00:29:16 -0300

    feat: VerificationTokenRepository method to delete all used or expired method

--- backend/backend/src/main/java/com/usermanager/manager/repository/VerificationTokenRepository.java
+++ backend/backend/src/main/java/com/usermanager/manager/repository/VerificationTokenRepository.java
@@ -1,11 +1,14 @@
 package com.usermanager.manager.repository;
 
+import java.time.Instant;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.usermanager.manager.model.user.User;
 import com.usermanager.manager.model.verification.VerificationToken;
@@ -17,4 +20,9 @@ public interface VerificationTokenRepository extends JpaRepository<VerificationT
     @Query("SELECT vt FROM VerificationToken vt WHERE vt.user = ?1 ORDER BY vt.creationDate DESC FETCH first 1 rows only")
     Optional<VerificationToken> findByUserMostRecent(User user);
 
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM VerificationToken vt WHERE vt.activated = true OR vt.creationDate < :threshold")
+    void deleteAllActivatedAndExpired(Instant threshold);
 }
