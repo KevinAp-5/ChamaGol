7381421ad375930d209ebf761640ba0b6fd559d7
KevinAp-5 <keven.clash3@gmail.com>
2025-05-19 23:38:01 -0300

    fix: removing webhook ID sequence conflit

--- backend/backend/src/main/java/com/usermanager/manager/model/webhook/WebhookEvent.java
+++ backend/backend/src/main/java/com/usermanager/manager/model/webhook/WebhookEvent.java
@@ -11,7 +11,6 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,22 +26,20 @@ import lombok.NoArgsConstructor;
 public class WebhookEvent {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "webhook_events_seq_gen")
-    @SequenceGenerator(name = "webhook_events_seq_gen", sequenceName = "webhook_events_seq", allocationSize = 1)
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "payload_json", columnDefinition = "TEXT")
     private String payloadJson;
-    
+
     @Column(name = "event_status")
     @Enumerated(EnumType.STRING)
     private EventStatus status; // PENDING, PROCESSED, ERROR
-    
+
     private ZonedDateTime receivedAt;
     private ZonedDateTime processedAt;
-    
+
     @Builder.Default
     private Integer retryCount = 0;
-    
+
 }
