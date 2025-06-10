212f70b148c80b52aa51a5cd47400f3175019c29
KevinAp-5 <keven.clash3@gmail.com>
2025-06-09 23:23:53 -0300

    fix: swagger profile only for dev or test env

--- backend/backend/src/main/java/com/usermanager/manager/infra/config/OpenApiConfig.java
+++ backend/backend/src/main/java/com/usermanager/manager/infra/config/OpenApiConfig.java
@@ -3,11 +3,13 @@ package com.usermanager.manager.infra.config;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
+@Profile("!prod")
 public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
