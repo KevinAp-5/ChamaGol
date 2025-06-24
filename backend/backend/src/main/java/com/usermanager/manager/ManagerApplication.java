db194a3f080e080316e33d2674c3982a453a5c8f
KevinAp-5 <keven.clash3@gmail.com>
2025-06-24 16:00:39 -0300

    chore: todos

--- backend/backend/src/main/java/com/usermanager/manager/ManagerApplication.java
+++ backend/backend/src/main/java/com/usermanager/manager/ManagerApplication.java
@@ -10,6 +10,9 @@ public class ManagerApplication {
 		SpringApplication.run(ManagerApplication.class, args);
 		// TODO: adicionar envio de notificações mesmo com app fechado
 		// TODO: criar um Scheduler para ir limpando as entidades antigas
+		// TODO: arrumar o STATUS inative mesmo quando o usuário confirma o email.
+
+		// TODO: mudar aplicação para portugues, incluindo mensagens de erros, e os email
 	}
 
 }
