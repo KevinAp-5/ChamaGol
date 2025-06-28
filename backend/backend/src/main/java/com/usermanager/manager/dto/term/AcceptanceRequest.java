b9c418c3bf39e95347a9b2bc114159c6ee2a1e27
KevinAp-5 <keven.clash3@gmail.com>
2025-06-28 11:22:13 -0300

    fix: AcceptanceRequest removed annotation

--- backend/backend/src/main/java/com/usermanager/manager/dto/term/AcceptanceRequest.java
+++ backend/backend/src/main/java/com/usermanager/manager/dto/term/AcceptanceRequest.java
@@ -1,7 +1,5 @@
 package com.usermanager.manager.dto.term;
 
-import jakarta.validation.constraints.NotNull;
-
-public record AcceptanceRequest(@NotNull Boolean isAdult) {
+public record AcceptanceRequest(Boolean isAdult) {
 
 }
