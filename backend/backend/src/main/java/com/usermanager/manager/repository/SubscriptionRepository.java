c2ad3fcc91e624fbca4fc9b2df9ccbf555c349f8
KevinAp-5 <keven.clash3@gmail.com>
2025-06-30 01:17:29 -0300

    feat: SubscriptionRepository findByUserId method

--- backend/backend/src/main/java/com/usermanager/manager/repository/SubscriptionRepository.java
+++ backend/backend/src/main/java/com/usermanager/manager/repository/SubscriptionRepository.java
@@ -2,14 +2,18 @@ package com.usermanager.manager.repository;
 
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.usermanager.manager.model.subscription.SubscriptionControl;
+import com.usermanager.manager.model.user.User;
 
 public interface SubscriptionRepository extends JpaRepository<SubscriptionControl, Long>{
 
     @Query("SELECT s from SubscriptionControl s WHERE s.expirationDate < :date")
     List<SubscriptionControl> findAllExpired(ZonedDateTime actualDate);
+
+    Optional<SubscriptionControl> findByUserId(User user); 
 }
