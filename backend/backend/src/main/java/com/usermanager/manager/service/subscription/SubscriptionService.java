97201cd4da339c0ffadd720d64ccb59c72eae76c
KevinAp-5 <keven.clash3@gmail.com>
2025-06-29 23:01:13 -0300

    feat: SubscriptionService interface methods

--- backend/backend/src/main/java/com/usermanager/manager/service/subscription/SubscriptionService.java
+++ backend/backend/src/main/java/com/usermanager/manager/service/subscription/SubscriptionService.java
@@ -1,5 +1,13 @@
 package com.usermanager.manager.service.subscription;
 
+import com.usermanager.manager.model.subscription.SubscriptionControl;
+import com.usermanager.manager.model.user.User;
+
+import jakarta.validation.constraints.NotNull;
+
 public interface SubscriptionService {
 
+    public SubscriptionControl createSubscriptionControl(@NotNull User user);
+
+    public void updateSubscriptions();
 }
