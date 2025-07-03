8bf8ba80067782c6e38f597bead56aa24034eff6
KevinAp-5 <keven.clash3@gmail.com>
2025-07-02 22:23:48 -0300

    feat: UserController new methods

--- backend/backend/src/main/java/com/usermanager/manager/controller/UserController.java
+++ backend/backend/src/main/java/com/usermanager/manager/controller/UserController.java
@@ -1,13 +1,8 @@
 package com.usermanager.manager.controller;
 
-import io.swagger.v3.oas.annotations.*;
-import io.swagger.v3.oas.annotations.media.*;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.responses.*;
-import io.swagger.v3.oas.annotations.tags.Tag;
-
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,17 +10,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.usermanager.manager.dto.common.ResponseMessage;
 import com.usermanager.manager.dto.user.DeleteByLoginDTO;
+import com.usermanager.manager.dto.user.ProUserDTO;
 import com.usermanager.manager.dto.user.SubscriptionDTO;
 import com.usermanager.manager.dto.user.UserDTO;
 import com.usermanager.manager.dto.user.UserResponseDTO;
 import com.usermanager.manager.model.user.User;
 import com.usermanager.manager.service.user.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -63,6 +67,15 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserDTO>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @Operation(summary = "Listar usuários paginados")
+    @ApiResponse(responseCode = "200", description = "Lista de usuários paginados")
+    @GetMapping("/page")
+    public ResponseEntity<Page<UserDTO>> getUsersPage(
+        @Parameter(description = "Número da página (0-based)") @RequestParam(defaultValue = "0") int page,
+        @Parameter(description = "Tamanho da página") @RequestParam(defaultValue="10") int size) {
+        return ResponseEntity.ok(userService.getUsersPage(page, size));
     }
 
     @Operation(summary = "Buscar usuário por ID")
@@ -114,4 +127,9 @@ public class UserController {
         log.info("user {}", user);
         return ResponseEntity.ok(new SubscriptionDTO(user.getSubscription().getValue()));
     }
+
+    @GetMapping("pro")
+    public ResponseEntity<List<ProUserDTO>> getsUsersPro() {
+            return ResponseEntity.ok(userService.getUsersProPage());
+        }
 }
