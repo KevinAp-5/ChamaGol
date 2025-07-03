3d476805d26f78f87db595d83078db7663cc01e9
KevinAp-5 <keven.clash3@gmail.com>
2025-07-02 22:21:40 -0300

    feat: UserRepository new method

--- backend/backend/src/main/java/com/usermanager/manager/repository/UserRepository.java
+++ backend/backend/src/main/java/com/usermanager/manager/repository/UserRepository.java
@@ -1,19 +1,23 @@
 package com.usermanager.manager.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
+import com.usermanager.manager.enums.Subscription;
 import com.usermanager.manager.model.user.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>{
 
     Optional<UserDetails> findByLogin(String login);
 
     boolean existsByLogin(String login);
 
     Optional<User> findUserByLogin(String login);
+
+    List<User> findAllBySubscription(Subscription subscription);
 }
