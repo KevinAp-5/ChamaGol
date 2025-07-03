ec9178a96368051e16ab04b97c4cd30d2d280a05
KevinAp-5 <keven.clash3@gmail.com>
2025-07-02 22:20:08 -0300

    chore: removed unused import

--- backend/backend/src/main/java/com/usermanager/manager/infra/config/FlywayRepairBean.java
+++ backend/backend/src/main/java/com/usermanager/manager/infra/config/FlywayRepairBean.java
@@ -1,7 +1,6 @@
 package com.usermanager.manager.infra.config;
 
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
