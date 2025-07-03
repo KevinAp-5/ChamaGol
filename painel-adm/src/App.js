1546b286b8513dc527453340f3dfd955e6667da1
KevinAp-5 <keven.clash3@gmail.com>
2025-07-02 22:25:51 -0300

    feat: App.js

--- painel-adm/src/App.js
+++ painel-adm/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Login from "./pages/login";
+import Dashboard from "./pages/Dashboard";
+import SendSignal from "./pages/SendSignal";
+import Terms from "./pages/Terms";
+import Signals from "./pages/Signals";
+import Users from "./pages/Users";
+import ProUsers from "./pages/ProUsers";
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {!isLogged ? (
+          <Route
+            path="*"
+            element={<Login onLogin={() => setIsLogged(true)} />}
+          />
+        ) : (
+          <>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/enviar-sinal" element={<SendSignal />} />
+            <Route path="/termos" element={<Terms />} />
+            <Route path="/sinais" element={<Signals />} />
+            <Route path="/usuarios" element={<Users />} />
+            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/usuarios-pro" element={<ProUsers />} />
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
