49e09443a9542b7a9fe4719b41e1ba3228b74c0e
KevinAp-5 <keven.clash3@gmail.com>
2025-07-13 22:57:42 -0300

    feat: SubscriptionRepository new methods

--- backend/backend/src/main/java/com/usermanager/manager/repository/SubscriptionRepository.java
+++ backend/backend/src/main/java/com/usermanager/manager/repository/SubscriptionRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.usermanager.manager.model.subscription.SubscriptionControl;
 import com.usermanager.manager.model.user.User;
@@ -15,5 +16,11 @@ public interface SubscriptionRepository extends JpaRepository<SubscriptionContro
     @Query("SELECT s from SubscriptionControl s WHERE s.expirationDate < :date")
     List<SubscriptionControl> findAllExpired(ZonedDateTime actualDate);
 
-    Optional<SubscriptionControl> findByUserId(User user); 
+    Optional<SubscriptionControl> findByUserId(User user);
+
+    @Query("SELECT s from SubscriptionControl s WHERE s.expirationDate BETWEEN :now AND :threeDaysAhead")
+    List<SubscriptionControl> findBySubscriptionEnding(@Param("now") ZonedDateTime now, @Param("ThreeDaysAhead") ZonedDateTime threeDaysAhead);
+
+    @Query("SELECT s from SubscriptionControl s WHERE s.expirationDate < :today")
+    List<SubscriptionControl> findExpiredSubscription(@Param("today") ZonedDateTime today);
 }
