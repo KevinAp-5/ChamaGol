2cf22316e1e80de8eb7d9b7f14b155d9e40b131d
KevinAp-5 <keven.clash3@gmail.com>
2025-08-15 21:40:00 -0300

    feat: sale entity

---
+++ backend/backend/src/main/java/com/usermanager/manager/model/sale/Sale.java
@@ -0,0 +1,50 @@
+package com.usermanager.manager.model.sale;
+
+import java.math.BigDecimal;
+import java.time.ZonedDateTime;
+
+import com.usermanager.manager.enums.Status;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "Sale")
+@Table(name = "sale")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, precision = 2, scale = 2)
+    private BigDecimal salePrice;
+
+    @Column(nullable = false)
+    // UserAmout = 0 para ofertas que contem tempo de expiração
+    private Integer userAmount = 0;
+
+    @Column(nullable = false)
+    private ZonedDateTime saleExpiration;
+
+    @Column(nullable = false)
+    private Integer userSubscriptionTime = 30;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.ACTIVE;
+}
