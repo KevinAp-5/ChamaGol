edbfe8c3130fdc54fdfac232bd43d6ffc3243e05
KevinAp-5 <keven.clash3@gmail.com>
2025-08-15 21:39:34 -0300

    chore: comments

--- backend/backend/src/main/java/com/usermanager/manager/ManagerApplication.java
+++ backend/backend/src/main/java/com/usermanager/manager/ManagerApplication.java
@@ -9,15 +9,13 @@ public class ManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManagerApplication.class, args);
 		// TODO: adicionar envio de notificações mesmo com app fechado
-		// TODO: criar um Scheduler para ir limpando as entidades antigas
-
-
-		// TODO: arrumar Error interno no servidor: could not execute statement [ERROR: duplicate key value violates unique constraint \"users_login_key\"\n  Detail: Key (login)=(keven.clash3@gmail.com) already exists.] [insert into users (created_at,is_enabled,last_login,login,name,password,role,status,subscription,updated_at) values (?,?,?,?,?,?,?,?,?,?)]; SQL [insert into users (created_at,is_enabled,last_login,login,name,password,role,status,subscription,updated_at) values (?,?,?,?,?,?,?,?,?,?)]; constraint [users_login_key]
 
 
 		// TODO: inserir um default term of uso assim que a aplicação rodar, caso estiver ausente no banco de dados
 
 		// TODO: adicionar um controller para gerenciar os preços do produto + adicionar uma regra de assinatura trimensal,etc...
+
+		// TODO: adicionar um email quando o usuário ativar a assinatura VIP
 	}
 
 }
