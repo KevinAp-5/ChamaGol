219fb3a19b527c06c0c10d88a17b57da82556ddd
KevinAp-5 <keven.clash3@gmail.com>
2025-09-18 19:30:17 -0300

    fix: migration fix

--- backend/backend/src/main/resources/db/migration/V25__create_table_sale.sql
+++ backend/backend/src/main/resources/db/migration/V25__create_table_sale.sql
@@ -1,9 +1,9 @@
 CREATE TABLE sale (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    sale_price NUMERIC(2, 2) NOT NULL,
+    sale_price NUMERIC(10, 2) NOT NULL,
     user_amount INT NOT NULL DEFAULT 0,
-    sale_expiration TIMESTAMPTZ NOT NULL,
+    sale_expiration TIMESTAMPTZ,
     user_subscription_time INT NOT NULL DEFAULT 30,
     status VARCHAR(255) NOT NULL
 );
