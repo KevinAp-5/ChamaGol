19c1264c949f3b13381442615cb9f1a01ec730d5
KevinAp-5 <keven.clash3@gmail.com>
2025-09-20 21:18:17 -0300

    chore(migration): migration fix

--- backend/backend/src/main/resources/db/migration/V25__create_table_sale.sql
+++ backend/backend/src/main/resources/db/migration/V25__create_table_sale.sql
@@ -3,7 +3,10 @@ CREATE TABLE sale (
     name VARCHAR(255) NOT NULL,
     sale_price NUMERIC(10, 2) NOT NULL,
     user_amount INT NOT NULL DEFAULT 0,
+    used_amount INT NOT NULL,
     sale_expiration TIMESTAMPTZ,
+    creation_date TIMESTAMPTZ NOT NULL,
+    finished_date TIMESTAMPTZ,
     user_subscription_time INT NOT NULL DEFAULT 30,
     status VARCHAR(255) NOT NULL
 );
