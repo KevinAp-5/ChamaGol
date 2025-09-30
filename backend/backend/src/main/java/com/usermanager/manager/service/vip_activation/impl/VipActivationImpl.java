da0d8072c921bc4a265466fa625b73e405b34e2d
KevinAp-5 <keven.clash3@gmail.com>
2025-09-30 20:03:17 -0300

    feat: VipActivation  impl class plus methods

---
+++ backend/backend/src/main/java/com/usermanager/manager/service/vip_activation/impl/VipActivationImpl.java
@@ -0,0 +1,73 @@
+package com.usermanager.manager.service.vip_activation.impl;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.usermanager.manager.enums.Subscription;
+import com.usermanager.manager.model.VipActivation.VipActivation;
+import com.usermanager.manager.model.sale.Sale;
+import com.usermanager.manager.model.user.User;
+import com.usermanager.manager.repository.VipActivationRepository;
+import com.usermanager.manager.service.sale.SaleService;
+import com.usermanager.manager.service.subscription.SubscriptionService;
+import com.usermanager.manager.service.user.UserService;
+import com.usermanager.manager.service.vip_activation.VipActivationService;
+
+@Service
+public class VipActivationImpl implements VipActivationService {
+
+    private VipActivationRepository vipActivationRepository;
+    private UserService userService;
+    private SaleService saleService;
+    private SubscriptionService subscriptionService;
+
+    public VipActivationImpl(VipActivationRepository vipActivationRepository, UserService userService,
+            SaleService saleService, SubscriptionService subscriptionService) {
+        this.vipActivationRepository = vipActivationRepository;
+        this.userService = userService;
+        this.saleService = saleService;
+        this.subscriptionService = subscriptionService;
+    }
+
+    @Override
+    @Transactional
+    public VipActivation createVipActivation(User user) {
+        Optional<Sale> sale = saleService.getActiveSale();
+        VipActivation vipActivation = VipActivation.builder()
+                .userId(user)
+                .saleId(sale.orElse(null))
+                .build();
+
+        return vipActivationRepository.save(vipActivation);
+    }
+
+    @Transactional
+    public VipActivation nextVipActivation() {
+        VipActivation vipActivation = vipActivationRepository.findFirstByProcessedFalseOrderByCreationDateAsc();
+        if (vipActivation == null)
+            return null;
+        User user = vipActivation.getUserId();
+        Sale sale = vipActivation.getSaleId();
+
+        user.setSubscription(Subscription.VIP);
+        user.setUpdatedAt(ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")));
+        if (sale != null)
+            saleService.useSale();
+
+        Integer subscriptionTime = (sale != null) ? sale.getUserSubscriptionTime() : Integer.valueOf(30);
+        createSubscriptionControl(user, subscriptionTime);
+        userService.save(user);
+
+        vipActivation.setProcessed(true);
+        return vipActivationRepository.save(vipActivation);
+    }
+
+    @Transactional
+    private void createSubscriptionControl(User user, Integer subscriptionDays) {
+        subscriptionService.createSubscriptionControl(user, subscriptionDays.intValue());
+    }
+}
