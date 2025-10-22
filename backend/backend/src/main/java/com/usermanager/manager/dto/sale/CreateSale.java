432f06b897f6cfb6805c37399f7527b84bd8156e
KevinAp-5 <keven.clash3@gmail.com>
2025-10-22 12:35:04 -0300

    fix: CreateSale DTI fixed

--- backend/backend/src/main/java/com/usermanager/manager/dto/sale/CreateSale.java
+++ backend/backend/src/main/java/com/usermanager/manager/dto/sale/CreateSale.java
@@ -3,18 +3,15 @@ package com.usermanager.manager.dto.sale;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 
-import com.google.firebase.database.annotations.NotNull;
-
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record CreateSale(
-    @NotBlank String name, 
+    @NotBlank String name,
     @NotNull @Positive BigDecimal salePrice,
     @NotNull @Positive Integer userAmount,
     ZonedDateTime saleExpiration,
-    @NotNull @Positive Integer userSubscriptionTime
-
-    ) {
-
-}
+    @NotNull @Positive Integer userSubscriptionTime,
+    @NotNull boolean userUnlimited
+) {}
