42c1d4e1e643627f8c21bb8d9444e8746deb9753
KevinAp-5 <keven.clash3@gmail.com>
2025-10-22 12:36:05 -0300

    feat: SaleReposutory new method

--- backend/backend/src/main/java/com/usermanager/manager/repository/SaleRepository.java
+++ backend/backend/src/main/java/com/usermanager/manager/repository/SaleRepository.java
@@ -1,5 +1,7 @@
 package com.usermanager.manager.repository;
 
+import java.time.ZonedDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,4 +14,6 @@ import com.usermanager.manager.model.sale.Sale;
 public interface SaleRepository extends JpaRepository<Sale, Long>{
 
     Optional<Sale> findFirstByStatus(Status status);
+
+    List<Sale> findAllByStatusAndSaleExpirationBefore(Status active, ZonedDateTime now);
 }
