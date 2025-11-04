06470f798c9443b99663ac32f2bd6a07794d94f7
KevinAp-5 <keven.clash3@gmail.com>
2025-11-03 21:37:27 -0300

    fix: sinalMapper fix

--- backend/backend/src/main/java/com/usermanager/manager/mappers/SignalMapper.java
+++ backend/backend/src/main/java/com/usermanager/manager/mappers/SignalMapper.java
@@ -7,6 +7,9 @@ import com.usermanager.manager.dto.signal.SignalCreated;
 import com.usermanager.manager.dto.signal.SignalDTO;
 import com.usermanager.manager.model.signal.Signal;
 
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SignalMapper {
 
@@ -14,4 +17,15 @@ public interface SignalMapper {
     SignalDTO entityToSignalDTO(Signal data);
     Signal signalCreatedToEntity(SignalCreated data);
     SignalCreated entityToSignalCreated(Signal data);
+
+    default ZonedDateTime map(Integer days) {
+        if (days == null) return null;
+        return ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).plusDays(days);
+    }
+
+    default Integer map(ZonedDateTime dateTime) {
+        if (dateTime == null) return null;
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"));
+        return (int) java.time.Duration.between(now, dateTime).toDays();
+    }
 }
