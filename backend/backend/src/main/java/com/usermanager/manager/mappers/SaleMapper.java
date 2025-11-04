4230082d36fc9fde26a8ad71b406d16e836ffdce
KevinAp-5 <keven.clash3@gmail.com>
2025-11-03 21:43:55 -0300

    fix: know fixing it for sure

--- backend/backend/src/main/java/com/usermanager/manager/mappers/SaleMapper.java
+++ backend/backend/src/main/java/com/usermanager/manager/mappers/SaleMapper.java
@@ -1,5 +1,8 @@
 package com.usermanager.manager.mappers;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
@@ -10,4 +13,15 @@ import com.usermanager.manager.model.sale.Sale;
 public interface SaleMapper {
     Sale CreateSaleToSale(CreateSale createSale);
     CreateSale SaleToCreateSale(Sale sale);
+
+    default ZonedDateTime map(Integer days) {
+        if (days == null) return null;
+        return ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).plusDays(days);
+    }
+
+    default Integer map(ZonedDateTime dateTime) {
+        if (dateTime == null) return null;
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"));
+        return (int) java.time.Duration.between(now, dateTime).toDays();
+    }
 }
--- backend/backend/src/main/java/com/usermanager/manager/mappers/SignalMapper.java
+++ backend/backend/src/main/java/com/usermanager/manager/mappers/SignalMapper.java
@@ -7,25 +7,11 @@ import com.usermanager.manager.dto.signal.SignalCreated;
 import com.usermanager.manager.dto.signal.SignalDTO;
 import com.usermanager.manager.model.signal.Signal;
 
-import java.time.ZonedDateTime;
-import java.time.ZoneId;
-
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SignalMapper {
 
     Signal signalDTOToEntity(SignalDTO data);
     SignalDTO entityToSignalDTO(Signal data);
     Signal signalCreatedToEntity(SignalCreated data);
-    SignalCreated entityToSignalCreated(Signal data);
-
-    default ZonedDateTime map(Integer days) {
-        if (days == null) return null;
-        return ZonedDateTime.now(ZoneId.of("America/Sao_Paulo")).plusDays(days);
-    }
-
-    default Integer map(ZonedDateTime dateTime) {
-        if (dateTime == null) return null;
-        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"));
-        return (int) java.time.Duration.between(now, dateTime).toDays();
-    }
+    SignalCreated entityToSignalCreated(Signal data); 
 }
