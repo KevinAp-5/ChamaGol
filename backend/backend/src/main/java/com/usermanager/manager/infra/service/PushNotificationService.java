3d0c0d15060099c1065f72cb459c86c48fe3d3a0
KevinAp-5 <keven.clash3@gmail.com>
2026-03-01 22:13:34 -0300

    feat: PushNotificationService

---
+++ backend/backend/src/main/java/com/usermanager/manager/infra/service/PushNotificationService.java
@@ -0,0 +1,44 @@
+package com.usermanager.manager.infra.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.usermanager.manager.model.device.Device;
+import com.usermanager.manager.service.device.DeviceService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PushNotificationService {
+    private final DeviceService deviceService;
+
+    private final WebClient webClient = WebClient.create("https://exp.host");
+
+    public void sentToAll(String title, String body) {
+        List<Device> devices = deviceService.findAllByActiveTrue();
+
+        devices.forEach(device -> send(device.getPushToken(), title, body));
+    }
+
+    private void send(String token, String title, String body) {
+        Map<String, Object> payload = Map.of(
+            "to", token,
+            "title", title,
+            "body", body,
+            "sound", "default"
+        );
+
+        webClient.post()
+            .uri("/--/api/v2/push/send")
+            .bodyValue(payload)
+            .retrieve()
+            .bodyToMono(String.class)
+            .subscribe();
+    }
+}
