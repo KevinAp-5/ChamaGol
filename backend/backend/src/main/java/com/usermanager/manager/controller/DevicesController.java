f36c4c05032e60fb9407757a50badacab7d88103
KevinAp-5 <keven.clash3@gmail.com>
2026-03-31 21:42:03 -0300

    chore: removed dependency

--- backend/backend/src/main/java/com/usermanager/manager/controller/DevicesController.java
+++ backend/backend/src/main/java/com/usermanager/manager/controller/DevicesController.java
@@ -3,6 +3,7 @@ package com.usermanager.manager.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +11,6 @@ import com.usermanager.manager.dto.device.RegisterDeviceRequest;
 import com.usermanager.manager.model.user.User;
 import com.usermanager.manager.service.device.DeviceService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
