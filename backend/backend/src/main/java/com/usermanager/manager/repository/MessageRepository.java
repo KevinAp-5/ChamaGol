4ad0ed698a7abec4e4eaba42b9164f3acd35b42c
KevinAp-5 <keven.clash3@gmail.com>
2026-04-16 19:23:45 -0300

    feat: MessageReposity findAll pageable method and method to fetch messages greater than a given id ordered by the created at date

--- backend/backend/src/main/java/com/usermanager/manager/repository/MessageRepository.java
+++ backend/backend/src/main/java/com/usermanager/manager/repository/MessageRepository.java
@@ -1,6 +1,11 @@
 package com.usermanager.manager.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.usermanager.manager.model.message.Message;
@@ -8,4 +13,8 @@ import com.usermanager.manager.model.message.Message;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long>{
 
+    Page<Message> findAll(Pageable pageable);
+    
+    @Query("SELECT m FROM Message m WHERE m.id > :id ORDER BY m.createdAt ASC")
+    List<Message> findByIdGreaterThanOrderByCreatedAtAsc(Long id);
 }
