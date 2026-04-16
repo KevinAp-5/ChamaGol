29aa89882f39d2cf4ef7b897cd624edaf95982af
KevinAp-5 <keven.clash3@gmail.com>
2026-04-16 16:44:18 -0300

    feat: heartbeat

--- backend/backend/src/main/java/com/usermanager/manager/controller/HeartbeatController.java
+++ backend/backend/src/main/java/com/usermanager/manager/controller/HeartbeatController.java
@@ -9,19 +9,19 @@ import com.usermanager.manager.model.user.User;
 import com.usermanager.manager.websocket.presence.PresenceService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class HeartbeatController {
 
     private final PresenceService presenceService;
 
     @MessageMapping("/heartbeat")
     public void heartbeat(Authentication auth) {
-
         if (auth != null) {
             var user = (User) auth.getPrincipal();
-
             presenceService.heartbeat(user.getId());
         }
     }
