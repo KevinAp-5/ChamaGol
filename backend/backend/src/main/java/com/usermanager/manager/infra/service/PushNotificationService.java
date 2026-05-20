d38212aa8f29f904f4005aea4a9a59f8f2ba9acc
KevinAp-5 <keven.clash3@gmail.com>
2026-05-20 14:51:08 -0300

    chore: backend Logs on PushNotificationService

--- backend/backend/src/main/java/com/usermanager/manager/infra/service/PushNotificationService.java
+++ backend/backend/src/main/java/com/usermanager/manager/infra/service/PushNotificationService.java
@@ -25,22 +25,27 @@ public class PushNotificationService {
 
 
     public void sendToUsers(List<Long> usersIds, String title, String body) {
+        log.info("users received do send notification: {}, notification body: {}", usersIds, body);
         List<Device> devices = deviceService.findAllByUserIdIn(usersIds);
 
+        log.info("devices found to send notification: {}", devices);
         for (Device device: devices) {
+            String devicePushToken = device.getPushToken();
+            log.info("device push Token: {}", devicePushToken);
             Map<String, Object> payload = Map.of(
-                "to", device.getPushToken(),
+                "to", devicePushToken,
                 "title", title,
                 "body", body,
                 "sound", "default"
             );
 
             try {
-                restClient.post()
+                var response = restClient.post()
                     .uri("/--/api/v2/push/send")
                     .body(payload)
                     .retrieve()
                     .toBodilessEntity();
+                log.info("Expo notification send response: {}", response);
             } catch (Exception e) {
                 log.info("Erro ao enviar push: {}", e.getMessage());
             }
